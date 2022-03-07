bdcd5351b44ed2af4e799f5483e78ddfaf0e00df
Şahin Akkaya <sahin@sahinakkaya.dev>
2022-03-07 12:34:36 +0300

    Patch right click paste

--- x.c
+++ x.c
@@ -743,6 +743,10 @@ brelease(XEvent *e)
 		return;
 	}
 
+	if (e->xbutton.button == Button3)
+		selpaste(NULL);
+	else if (e->xbutton.button == Button1)
+		mousesel(e, 1);
 	if (mouseaction(e, 1))
 		return;
 	if (btn == Button1)
